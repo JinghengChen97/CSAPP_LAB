dbae2782e35a1b4b393de8ab41998b59e86031f0
JinghengChen97 <GDUT_jinghengchen@126.com>
2020-12-19 15:53:50 +0800

    Tinyweb:first commit.

---
+++ TinyWeb/cmake-build-debug/CMakeFiles/TinyWeb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TinyWeb.dir/csapp.c.o"
+  "CMakeFiles/TinyWeb.dir/tinyweb.c.o"
+  "TinyWeb"
+  "TinyWeb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TinyWeb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
